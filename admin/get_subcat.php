f302318a962161866fc7393261834b1122cd7921
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-05 22:57:49 +0530

    update

--- admin/get_subcat.php
+++ admin/get_subcat.php
@@ -3,11 +3,11 @@ include('include/config.php');
 if(!empty($_POST["cat_id"])) 
 {
  $id=intval($_POST['cat_id']);
-$query=mysql_query("SELECT * FROM subcategory WHERE categoryid=$id");
+$query=mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM subcategory WHERE categoryid=$id");
 ?>
 <option value="">Select Subcategory</option>
 <?php
- while($row=mysql_fetch_array($query))
+ while($row=mysqli_fetch_array($query))
  {
   ?>
   <option value="<?php echo htmlentities($row['id']); ?>"><?php echo htmlentities($row['subcategory']); ?></option>
