aa3c610ea1395d5cd0dbf6c1056582dffe862eae
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-05 22:53:48 +0530

    update

--- admin/change-password.php
+++ admin/change-password.php
@@ -13,11 +13,11 @@ $currentTime = date( 'd-m-Y h:i:s A', time () );
 
 if(isset($_POST['submit']))
 {
-$sql=mysql_query("SELECT password FROM  admin where password='".md5($_POST['password'])."' && username='".$_SESSION['alogin']."'");
-$num=mysql_fetch_array($sql);
+$sql=mysqli_query($GLOBALS["___mysqli_ston"], "SELECT password FROM  admin where password='".md5($_POST['password'])."' && username='".$_SESSION['alogin']."'");
+$num=mysqli_fetch_array($sql);
 if($num>0)
 {
- $con=mysql_query("update admin set password='".md5($_POST['newpassword'])."', updationDate='$currentTime' where username='".$_SESSION['alogin']."'");
+ $con=mysqli_query($GLOBALS["___mysqli_ston"], "update admin set password='".md5($_POST['newpassword'])."', updationDate='$currentTime' where username='".$_SESSION['alogin']."'");
 $_SESSION['msg']="Password Changed Successfully !!";
 }
 else
