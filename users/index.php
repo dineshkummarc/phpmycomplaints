72c3bbf4982e335f388ad1d6d38fecbffd7b7452
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-05 23:03:19 +0530

    update

--- users/index.php
+++ users/index.php
@@ -4,8 +4,8 @@ error_reporting(0);
 include("includes/config.php");
 if(isset($_POST['submit']))
 {
-$ret=mysql_query("SELECT * FROM users WHERE userEmail='".$_POST['username']."' and password='".md5($_POST['password'])."'");
-$num=mysql_fetch_array($ret);
+$ret=mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM users WHERE userEmail='".$_POST['username']."' and password='".md5($_POST['password'])."'");
+$num=mysqli_fetch_array($ret);
 if($num>0)
 {
 $extra="change-password.php";//
@@ -14,7 +14,7 @@ $_SESSION['id']=$num['id'];
 $host=$_SERVER['HTTP_HOST'];
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=1;
-$log=mysql_query("insert into userlog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['login']."','$uip','$status')");
+$log=mysqli_query($GLOBALS["___mysqli_ston"], "insert into userlog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['login']."','$uip','$status')");
 $uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 header("location:http://$host$uri/$extra");
 exit();
@@ -24,7 +24,7 @@ else
 $_SESSION['login']=$_POST['username'];	
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=0;
-mysql_query("insert into userlog(username,userip,status) values('".$_SESSION['login']."','$uip','$status')");
+mysqli_query($GLOBALS["___mysqli_ston"], "insert into userlog(username,userip,status) values('".$_SESSION['login']."','$uip','$status')");
 $errormsg="Invalid username or password";
 $extra="login.php";
 
@@ -38,11 +38,11 @@ if(isset($_POST['change']))
    $email=$_POST['email'];
     $contact=$_POST['contact'];
     $password=md5($_POST['password']);
-$query=mysql_query("SELECT * FROM users WHERE userEmail='$email' and contactNo='$contact'");
-$num=mysql_fetch_array($query);
+$query=mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM users WHERE userEmail='$email' and contactNo='$contact'");
+$num=mysqli_fetch_array($query);
 if($num>0)
 {
-mysql_query("update users set password='$password' WHERE userEmail='$email' and contactNo='$contact' ");
+mysqli_query($GLOBALS["___mysqli_ston"], "update users set password='$password' WHERE userEmail='$email' and contactNo='$contact' ");
 $msg="Password Changed Successfully";
 
 }
